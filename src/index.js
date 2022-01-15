0c26a477c2d8d6ac8cc1b1b4958ebb895c8cec33
ArtemFH <artembereza1@mail.ru>
2022-01-15 23:53:47 +0700

    update

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-import App from './App';
-import React from 'react';
-import ReactDOM from 'react-dom';
+import App from "./App";
+import React from "react";
+import ReactDOM from "react-dom";
 import {BrowserRouter} from "react-router-dom";
-import {StyledEngineProvider} from '@mui/material/styles';
+import {StyledEngineProvider} from "@mui/material/styles";
 
 ReactDOM.render(
     <React.StrictMode>
