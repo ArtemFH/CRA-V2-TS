eee1bae51d54e78d7e03b7dcc7eb873fcf7a95d9
ayyPls <saispaev55@mail.ru>
2022-01-25 18:07:21 +0700

    added useAppSelector and useAppDispatch hooks and usage example

---
+++ src/hooks/redux.ts
@@ -0,0 +1,16 @@
+import { AppDispatch, RootState } from "../store/store";
+import { useDispatch, TypedUseSelectorHook, useSelector } from "react-redux";
+import { bindActionCreators } from "@reduxjs/toolkit";
+import * as TemplatesActionsCreator from "../store/template/actions"
+
+
+
+export const useActions = () => {
+    const dispatch = useDispatch();
+    return bindActionCreators({
+        ...TemplatesActionsCreator,
+    }, dispatch)
+}
+
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
+export const useAppDispatch = () => useDispatch<AppDispatch>();
--- src/pages/IndexPage.tsx
+++ src/pages/IndexPage.tsx
@@ -1,11 +1,21 @@
-import {FC} from "react";
+import { FC, useEffect } from "react";
+import { useAppDispatch, useAppSelector } from "../hooks/redux";
+import { fetchTemplates } from "../store/template/actions";
 
 const Index: FC = () => {
+
+    //пример использования хуков useAppSelector и useAppDispatch
+    const dispatch = useAppDispatch()
+    const data = useAppSelector(state=>state.template.data)
+    useEffect(
+        () => {
+            dispatch(fetchTemplates())
+        }, []
+    )
     return (
         <>
 
         </>
     )
 }
-
 export default Index;
